34b85112938ed62fa5299b824e3304c9052e8c1a
Favorite <nameismtb@gmail.com>
2026-01-01 21:03:09 +0100

    Added RGB as well, now stores both

--- colours.py
+++ colours.py
@@ -6,7 +6,7 @@ def main():
     colours = []
     
     # Avoid duplicates
-    while len(colours) < 100000:
+    while len(colours) < 20000:
         # Random hex colour
         colour = "#{:06x}".format(random.randint(0, 0xFFFFFF))
         if colour not in colours:
@@ -16,8 +16,17 @@ def main():
     with open("colours.txt", "w") as file:
         for colour in colours:
             file.write(f"{colour}\n")
-    
-    print(f"Successfully generated {len(colours)} unique colours and saved them to colours.txt")
 
+    with open("coloursrgb.txt", "w") as file:
+        for colour in colours:
+            # Converting to RGB
+            hex_colour = colour[1:]
+            r = int(hex_colour[0:2], 16)
+            g = int(hex_colour[2:4], 16)
+            b = int(hex_colour[4:6], 16)
+            file.write(f"{r};{g};{b}\n")
+    
+    print(f"Successfully generated {len(colours)} unique colours.")
+    print(f"HEX colours saved to colours.txt, RGB saved to coloursrgb.txt")
 if __name__ == "__main__":
     main()
--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 An incredibly simple colour generator written in python.
 
-By default set to 100000 colours, easily adjusted.
+By default set to 20000 colours, easily adjusted.
+Will save HEX colours to colours.txt and RGB colours to coloursrgb.txt
